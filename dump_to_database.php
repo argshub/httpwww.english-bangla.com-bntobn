e079d639109a18d162ff4fdea5e056ac53515b32
pefinite <argshub@outlook.com>
2021-12-15 12:14:29 +0600

    15-12-2021

--- dump_to_database.php
+++ dump_to_database.php
@@ -1,5 +1,4 @@
 <?php
-require_once "word_processor.php";
 
 $worldOfWordsDatabase = new mysqli("127.0.0.1", "root", "", "mysql");
 $worldOfWordsDatabase->query("create database if not exists world_of_words CHARACTER SET utf8 COLLATE utf8_general_ci");
@@ -16,8 +15,34 @@ if ($file = fopen("btob.txt", "r")) {
         $wordId = insertWord($worldOfWordsDatabase->real_escape_string($line['word']));
         $definitions = $worldOfWordsDatabase->real_escape_string($line['meaning']);
 
-        $bTobTwo->query("insert into word_definitions (wordId, definitions) values ($wordId,'{$definitions}')");
-        print_r($counter++ . "\n");
+        if(!$bTobTwo->query("select * from word_definitions where wordId = {$wordId} and definitions = '{$definitions}'")->num_rows) {
+            $bTobTwo->query("insert into word_definitions (wordId, definitions) values ($wordId,'{$definitions}')");
+        }
+        print_r("\r000000000"  . $wordId . "->" . $bTobTwo->insert_id);
+
     }
     fclose($file);
+}
+
+function insertWord($word) {
+    // create table as order
+    $firstCharacterCode = mb_ord($word);
+    $firstCharacterCode = "character_" . $firstCharacterCode;
+    global $worldOfWordsDatabase;
+    if($query = $worldOfWordsDatabase->query("select * from $firstCharacterCode where word = '{$word}'")->fetch_array(MYSQLI_ASSOC)) {
+        return $query['wordId'];
+    } else {
+        if($lastWordId = $worldOfWordsDatabase->query("select max(wordId) as maxId from $firstCharacterCode")->fetch_array(MYSQLI_ASSOC)) {
+            $lastWordId = $lastWordId['maxId'];
+            if($lastWordId <= 0) {
+                $lastWordId = mb_ord($word) * 100000;
+            } else {
+                $lastWordId++;
+            }
+        } else {
+            $lastWordId = mb_ord($word) * 100000;
+        }
+        $worldOfWordsDatabase->query("insert into $firstCharacterCode (wordId, word) values ($lastWordId, '{$word}')");
+        return $lastWordId;
+    }
 }
